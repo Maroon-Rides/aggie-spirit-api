2fda10f14d099ffcdbf02f3e815a3f70a834cdd7
bwees <brandonwees@gmail.com>
2023-10-05 16:58:45 -0500

    fix example to be new format for getRouteInfo

--- examples/example.js
+++ examples/example.js
@@ -1,7 +1,9 @@
 import { getRouteInfo, getRoutes, RouteGroup } from '../src/index.js';
 
 getRoutes(RouteGroup.ALL).then((routes) => {
-    getRouteInfo(routes).then((routeInfo) => {
+
+    getRouteInfo(routes.map((r) => r.key)).then((routeInfo) => {
         console.log(routeInfo)
     })
+    
 })
