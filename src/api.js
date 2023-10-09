423f7618271503abd24285fa920d2fee0b3e8982
bwees <brandonwees@gmail.com>
2023-10-08 22:12:34 -0500

    connection is not handled by default anymore, connection reuse much simpler

--- src/api.js
+++ src/api.js
@@ -18,11 +18,10 @@ export const RouteGroup = {
  * Gets the routes for the given groups
  * @param {RouteGroup} groups groups to provide routes for, either array of RouteGroup or single RouteGroup
  * @param {MapConnection} connection MapConnection to use
- * @param {boolean} handleConnection should the connection be handled by this function (open and close)
  * @returns list of routes and their info, grouped by the groups given
  */
-export async function getRoutesByGroup(groups, connection = new MapConnection(), handleConnection = true) {
-    if (handleConnection) await connection.connect();
+export async function getRoutesByGroup(groups, connection = new MapConnection()) {
+    if (connection.autoHandle) await connection.connect();
 
     var routeGroups = {}
     
@@ -56,7 +55,7 @@ export async function getRoutesByGroup(groups, connection = new MapConnection(),
         }
     }
 
-    if (handleConnection) connection.close()
+    if (connection.autoHandle) connection.close()
 
     return routeGroups
 }
@@ -65,17 +64,16 @@ export async function getRoutesByGroup(groups, connection = new MapConnection(),
  * 
  * @param {String} routeName the short name of the route to get (e.g. 47-48, 04, etc)
  * @param {MapConnection} connection MapConnection to use
- * @param {boolean} handleConnection should the connection be handled by this function (open and close)
  * @returns 
  */
-export async function getRouteByName(routeName, connection = new MapConnection(), handleConnection = true) {
-    if (handleConnection) await connection.connect();
+export async function getRouteByName(routeName, connection = new MapConnection()) {
+    if (connection.autoHandle) await connection.connect();
 
     var route = await connection.send("GetRoute", [routeName])
     route.routeInfo = await getRouteInfo(route.key, connection, false)
     route.patternPoints = await getRoutePatternPoints(route.key, connection, false)
 
-    if (handleConnection) connection.close()
+    if (connection.autoHandle) connection.close()
 
     return route
 }
@@ -85,15 +83,14 @@ export async function getRouteByName(routeName, connection = new MapConnection()
  * This is automatically called by getRoutesByGroup and getRoutesByName and is returned in the routeInfo field
  * @param {String} routeKey key of the route to get info for
  * @param {MapConnection} connection MapConnection to use
- * @param {boolean} handleConnection should the connection be handled by this function (open and close)
  * @returns extended route info, includes: waypoints, stops, and route color
  */
-export async function getRouteInfo(routeKey, connection = new MapConnection(), handleConnection = true) {
-    if (handleConnection) await connection.connect()
+export async function getRouteInfo(routeKey, connection = new MapConnection()) {
+    if (connection.autoHandle) await connection.connect()
 
     var routeInfo = await connection.send("GetPatternPaths", [routeKey])
 
-    if (handleConnection) connection.close()
+    if (connection.autoHandle) connection.close()
     return routeInfo
 }
 
@@ -102,15 +99,14 @@ export async function getRouteInfo(routeKey, connection = new MapConnection(), h
  * This is automatically called by getRoutesByGroup and getRoutesByName and is returned in the patternPoints field
  * @param {String} routeKey key of the route to get info for
  * @param {MapConnection} connection MapConnection to use
- * @param {boolean} handleConnection should the connection be handled by this function (open and close)
  * @returns extended route info, includes: map color and icon info and extendied direction info
  */
-export async function getRoutePatternPoints(routeKey, connection = new MapConnection(), handleConnection = true) {
-    if (handleConnection) await connection.connect()
+export async function getRoutePatternPoints(routeKey, connection = new MapConnection()) {
+    if (connection.autoHandle) await connection.connect()
 
     var patternPoints = await connection.send("GetPatternPoints", [routeKey])
 
-    if (handleConnection) connection.close()
+    if (connection.autoHandle) connection.close()
     return patternPoints
 }
 
@@ -118,15 +114,14 @@ export async function getRoutePatternPoints(routeKey, connection = new MapConnec
  * Gets the active busses on the route given
  * @param {String} routeName the short name of the route to get busses for (e.g. 47-48, 04, etc)
  * @param {MapConnection} connection MapConnection to use
- * @param {boolean} handleConnection should the connection be handled by this function (open and close)
  * @returns an array of bus information for the active busses on the route
  */
-export async function getRouteBuses(routeName, connection = new MapConnection(), handleConnection = true) {
-    if (handleConnection) await connection.connect()
+export async function getRouteBuses(routeName, connection = new MapConnection()) {
+    if (connection.autoHandle) await connection.connect()
 
     var busses = await connection.send("GetBuses", [routeName])
 
-    if (handleConnection) connection.close()
+    if (connection.autoHandle) connection.close()
     return busses
 }
 
