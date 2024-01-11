29bc9055ac459ad137d53e3c0c2d929de380261c
bwees <brandonwees@gmail.com>
2024-01-11 12:29:19 -0500

    package updates

--- examples/example.js
+++ examples/example.js
@@ -1,4 +1,4 @@
-import { getStopEstimates, getAuthentication, getBaseData, getNearbyRoutes, getPatternPaths } from "../src/index.js";
+import { getStopEstimates, getAuthentication } from "../src/index.js";
 
 getAuthentication().then(async (auth) => {
     console.log(await getStopEstimates("0400", new Date(), auth))
