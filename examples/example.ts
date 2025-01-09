2e0c53db5cc8cba2eac127a0f5f675325bf0118c
bwees <brandonwees@gmail.com>
2025-01-08 21:16:46 -0500

    fix example

--- examples/example.ts
+++ examples/example.ts
@@ -1,4 +1,4 @@
-import { getStopEstimates, findBusStops, getBaseData } from "../dist/index.js";
+import { getStopEstimates, findBusStops, getBaseData } from "aggie-spirit-api";
 
 // you must provide your own authentication function and valid header to authenticate with the API
 const auth = {
